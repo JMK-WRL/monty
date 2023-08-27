4dce0c5cb5cd27bc7894e84b7f58e4c3a1c7f80f
Jonathan <jonathankyule2@gmail.com>
2023-08-27 14:35:31 -0700

    compilation errors

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include "monty.h"
 
+int main(int argc, char *argv[]);
+int process_line(char *line, stack_t **stack, unsigned int line_number);
+void cleanup(stack_t **stack);
+
 /**
  * main - monty interpretor
  * @argc: arguments
- * @argv: location
- * Return: 0 (success)
+ * @argv: files
+ * Return: 0 (success))
  */
 
 int main(int argc, char *argv[])
@@ -12,8 +16,6 @@ int main(int argc, char *argv[])
 	char line[100];
 	unsigned int line_number = 0;
 	FILE *file;
-	char opcode[100];
-	int value;
 	stack_t *stack = NULL;
 
 	if (argc != 2)
@@ -25,59 +27,77 @@ int main(int argc, char *argv[])
 	file = fopen(argv[1], "r");
 	if (!file)
 	{
-		fprintf(stderr, "Error:  Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
 	while (fgets(line, sizeof(line), file))
 	{
 		line_number++;
-		if (scanf(line, "%99s", opcode) == 1)
+		if (process_line(line, &stack, line_number) == -1)
 		{
-			if (strcmp(opcode, "push") == 0)
+			cleanup(&stack);
+			fclose(file);
+			return (EXIT_FAILURE);
+		}
+	}
+
+	cleanup(&stack);
+	fclose(file);
+	return (EXIT_SUCCESS);
+}
+
+int process_line(char *line, stack_t **stack, unsigned int line_number)
+{
+	char opcode[100];
+	int value;
+
+	if (sscanf(line, "%99s", opcode) == 1)
+	{
+		if (strcmp(opcode, "push") == 0)
+		{
+			if (sscanf(line, "%*s %d", &value) == 1)
 			{
-				if (scanf(line, "%*s %d", &value) == 1)
-				{
-					push(&stack, value);
-				}
-				else
-				{
-					fprintf(stderr, "L%d: usage: push integer\n", line_number);
-					exit(EXIT_FAILURE);
-				}
+				push(stack, value);
 			}
 			else
 			{
-				instruction_t instruction;
-
-				if (strcmp(opcode, "pall") == 0)
-					instruction.f = pall;
-				else if (strcmp(opcode, "pint") == 0)
-					instruction.f = pint;
-				else if (strcmp(opcode, "pop") == 0)
-					instruction.f = pop;
-				else if (strcmp(opcode, "swap") == 0)
-					instruction.f = swap;
-				else if (strcmp(opcode, "add") == 0)
-					instruction.f = add;
-				else if (strcmp(opcode, "nop") == 0)
-					instruction.f = nop;
-				else
-				{
-					fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-					exit(EXIT_FAILURE);
-				}
-				instruction.opcode = opcode;
-				instruction.f(&stack, line_number);
+				fprintf(stderr, "L%d: usage: push integer\n", line_number);
+				return (-1);
 			}
 		}
+		else
+		{
+			instruction_t instruction;
+			
+			if (strcmp(opcode, "pall") == 0)
+				instruction.f = pall;
+			else if (strcmp(opcode, "pint") == 0)
+				instruction.f = pint;
+			else if (strcmp(opcode, "pop") == 0)
+				instruction.f = pop;
+			else if (strcmp(opcode, "swap") == 0)
+				instruction.f = swap;
+			else if (strcmp(opcode, "add") == 0)
+				instruction.f = add;
+			else if (strcmp(opcode, "nop") == 0)
+				instruction.f = nop;
+			else
+			{
+				fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+				exit(EXIT_FAILURE);
+			}
+			instruction.opcode = opcode;
+			instruction.f(stack, line_number);
+		}
 	}
-	fclose(file);
-
-	while (stack != NULL)
-	{
-		pop(&stack, line_number);
-	}
-
 	return (0);
+}
+
+void cleanup(stack_t **stack)
+{
+	while (*stack != NULL)
+	{
+		pop(stack, 0);
+	}
 }
