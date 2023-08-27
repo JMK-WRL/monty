7ba80766d6f29a8e7110b248b9fb3e8e34549523
Jonathan <jonathankyule2@gmail.com>
2023-08-27 13:48:28 -0700

    push

--- main.c
+++ main.c
@@ -13,9 +13,8 @@ int main(int argc, char *argv[])
 {
 	char line[100];
 	unsigned int line_number = 0;
-	FILE *file = fopen(argv[1], "r");
-	char *opcode;
-	char *value_str;
+	FILE *file;
+	char opcode[100];
 	int value;
 
 	if (argc != 2)
@@ -24,30 +23,32 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
+	file = fopen(argv[1], "r");
 	if (!file)
 	{
-		fprintf(stderr, "Error: Can't opne file %s\n", argv[1]);
+		fprintf(stderr, "Error:  Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
 	while (fgets(line, sizeof(line), file))
 	{
 		line_number++;
-		opcode = strtok(line, " \t\n");
-		if (opcode)
+		if (scanf(line, "%99s", opcode) == 1)
 		{
 			if (strcmp(opcode, "push") == 0)
 			{
-				value_str = strtok(NULL, " \t\n");
-				if (value_str)
+				if (scanf(line, "%*s %d", &value) == 1)
 				{
-					value = atoi(value_str);
 					push(&stack, value);
-				} else {
+				}
+				else
+				{
 					fprintf(stderr, "L%d: usage: push integer\n", line_number);
 					exit(EXIT_FAILURE);
 				}
-			} else {
+			}
+			else
+			{
 				instruction_t instruction;
 
 				if (strcmp(opcode, "pall") == 0)
@@ -62,7 +63,8 @@ int main(int argc, char *argv[])
 					instruction.f = add;
 				else if (strcmp(opcode, "nop") == 0)
 					instruction.f = nop;
-				else {
+				else
+				{
 					fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
 					exit(EXIT_FAILURE);
 				}
@@ -74,4 +76,3 @@ int main(int argc, char *argv[])
 	fclose(file);
 	return (0);
 }
-
